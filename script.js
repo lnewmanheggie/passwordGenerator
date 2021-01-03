08d1060707823eefe2b2e8ff7f1e4f3765384746
lnewmanheggie <lnewmanheggie@gmail.com>
2021-01-03 13:45:14 -0600

    Updated with a bootstrap input form

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var resetBtn = document.querySelector("#reset");
+const generateBtn = document.querySelector("#generate");
+const resetBtn = document.querySelector("#reset");
 
 // Function to generate a random password based on user specification.
-function randomPass(char, len) {
-  var charArray = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "1234567890", "!@#$%&*"];
-  var passChars = "";
+function randomPass(charT, len) {
+  const charArray = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "1234567890", "!@#$%&*"];
+  let passChars = "";
 
-  if (char.includes("u") || char.includes("all")) {
-    passChars += charArray[0];
-  }
-  if (char.includes("l") || char.includes("all")) {
-    passChars += charArray[1];
-  }
-  if (char.includes("n") || char.includes("all")) {
-    passChars += charArray[2];
-  }
-  if (char.includes("s") || char.includes("all")) {
-    passChars += charArray[3];
+  for (let i = 0; i < charT.length; i++) {
+    if (charT[i].value === "uppercase") {
+      passChars += charArray[0];
+    } 
+    if (charT[i].value === "lowercase") {
+      passChars += charArray[1];
+    }
+    if (charT[i].value === "numbers") {
+      passChars += charArray[2];
+    }
+    if (charT[i].value === "special") {
+      passChars += charArray[3];
+    }
   }
 
-  var passGenerated = "";
-  for (i = 0; i < len; i++) {
+  let passGenerated = "";
+  for (let i = 0; i < len; i++) {
     passGenerated += passChars.charAt(Math.floor(Math.random() * passChars.length));
   }
 
@@ -30,39 +32,47 @@ function randomPass(char, len) {
 
 // ask user for input and use that input to generate a random password.
 function generatePassword() {
-  var passLen = parseInt(prompt("How long would you like this password to be? (between 8 and 128 characters"));
+  const passLen = document.querySelector("#length").value;
   if (passLen < 8 || passLen > 128) {
     alert("please enter a number between 8 and 128");
-  }
-  else {
-    var charType = prompt("Which characters would you like in your password? (Type 'u' for uppercase, 'l' for lowercase, 'n' for number, 's' for special characters, or 'all' for all character types. You may type multiple letters.").toLowerCase();
-    if (charType.includes('u') || charType.includes('l') || charType.includes('n') || charType.includes('s') || charType.includes('all')) {
-      return randomPass(charType, passLen);
+  } else {
+    const charType = document.querySelectorAll('input[type="checkbox"]:checked');
+    return randomPass(charType, passLen); 
     }
-    else {
-      alert("Invalid character type.")
-    }
-  }
 }
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
+// Function to reset checkboxes
+function resetCheck() {
+  var items = document.getElementsByName('check');
+  for (var i = 0; i < items.length; i++) {
+      if (items[i].type == 'checkbox')
+          items[i].checked = false;
+  }
+}
+
 // Function to reset password
 function resetPassword() {
-  var passwordText = document.querySelector("#password");
+  const passwordText = document.querySelector("#password");
+  const passwordLength = document.querySelector("#length");
+  passwordLength.value = "";
   passwordText.value = "Reset successfully";
 
+  resetCheck();
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 resetBtn.addEventListener("click", resetPassword);
+
 
 
